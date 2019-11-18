4397c086b49d7a7517506d33dc7c94aa40c9a690
Raul Garcia (MSFT) <raul.garcia@microsoft.com>
2019-11-18 12:56:48 -0800

    PR Test

--- src/Test2a.c
+++ src/Test2a.c
@@ -1,8 +1,15 @@
 #include <test2/Test2a.h>
+#include <stdio.h>
 
 int test2()
 {
 	int x = 0;
 
 	return x;
+}
+
+void test3(char* lpzTest, int cbTest) 
+{ 
+	char[3] x;
+    memcpy(x, lpzTest, cbTest);
 }
